3d663b8e32c68fccb4c2ee6a19cc9747e0ba7789
James Hynes <jhynes@m-i.com.au>
2018-06-07 12:04:26 +1000

    Fix load order preventing Gem from being 'require'd.

--- lib/itglue/asset.rb
+++ lib/itglue/asset.rb
@@ -1,4 +1,9 @@
-Dir[File.join(File.dirname(__FILE__), 'asset/*.rb')].each {|file| require file }
+require File.join(File.dirname(__FILE__), 'asset/base.rb')
+require File.join(File.dirname(__FILE__), 'asset/configuration.rb')
+require File.join(File.dirname(__FILE__), 'asset/configuration_interface.rb')
+require File.join(File.dirname(__FILE__), 'asset/configuration_status.rb')
+require File.join(File.dirname(__FILE__), 'asset/configuration_type.rb')
+require File.join(File.dirname(__FILE__), 'asset/organization.rb')
 
 module ITGlue
   module Asset
